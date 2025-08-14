be9a1ef1047669c7f45364882730247796753f6e
fayiza <fayizamp3@gmail.com>
2025-08-14 10:42:51 +0530

    Revenue

--- erp-frontend/src/pages/frm/Profits.jsx
+++ erp-frontend/src/pages/frm/Profits.jsx
@@ -356,7 +356,7 @@ const Profits = () => {
             className="inline-flex items-center justify-center rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 sm:w-auto"
           >
             <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-            Add Profit
+            Add Revenue
           </button>
         </div>
       </div>
